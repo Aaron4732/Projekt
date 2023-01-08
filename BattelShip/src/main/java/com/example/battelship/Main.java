3b1587f1dec6d0e0df2878b999c1dd9b2a4f266d
aaron <aaron-dario.fries@stud.fh-campuswien.ac.at>
2023-01-08 13:43:05 +0100

    add comments to the classes - main - ships - userInput

--- BattelShip/src/main/java/com/example/battelship/Main.java
+++ BattelShip/src/main/java/com/example/battelship/Main.java
@@ -1,28 +1,33 @@
 package com.example.battelship;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         int gameBoardLength = 4;
+
+        //Symbols for the gamebord
         char water = '-';
         char ship = 'S';
         char hit = 'X';
         char miss = 'O';
 
+        //Set the number of ship availability ships
         int shipsWhitSize2 = 0;
         int shipsWhitSize3 = 1;
         int shipsWhitSize4 = 0;
         int shipsWhitSize5 = 0;
 
-
+        //Create the gamebord for player1
         GameBoard gameBoard1 = new GameBoard (gameBoardLength, water, ship, hit, miss, shipsWhitSize2, shipsWhitSize3, shipsWhitSize4, shipsWhitSize5 );
         gameBoard1.printGameBoard();
 
+        //play the game
         while (!gameBoard1.gameIsOver())
         {
+            //make a shut on gamebord1
             gameBoard1.userTarget();
             gameBoard1.printGameBoard();
         }
+        //print the "won" massage when the game is over
         System.out.println("You Won!");
     }
 }
--- BattelShip/src/main/java/com/example/battelship/Ships.java
+++ BattelShip/src/main/java/com/example/battelship/Ships.java
@@ -3,11 +3,18 @@ package com.example.battelship;
 import java.util.Random;
 
 public class Ships {
+    //A class for the ships to prevent methods for interaction with ships
 
     public static int[] generateShipCoordinates(int gameBoardLength) {
+        //the method generate random ship coordinates 1x1 and return them in a list
+
+        //create the list for return
         int[] coordinates = new int[2];
+
+        //generate a coordinationpoint for every dimension in the list "coordinates"
         for (int i = 0; i < coordinates.length; i++)
         {
+            //generate the point
             coordinates[i] = new Random().nextInt(gameBoardLength);
         }
         return coordinates;
