60a4dabe94e629a2a39b3bf8e17ed6709b4f68a2
beatrixdietl <113309245+beatrixdietl@users.noreply.github.com>
2023-01-10 22:24:51 +0100

    Update Main.java
    Changed comments a bit.

--- BattelShip/src/main/java/com/example/battelship/Main.java
+++ BattelShip/src/main/java/com/example/battelship/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int gameBoardLength = 4;
 
-        //Symbols for the gamebord
+        //Symbols for the gameboard
         char water = '-';
         char ship = 'S';
         char hit = 'X';
@@ -16,13 +16,13 @@ public class Main {
         int shipsSize4 = 0;
         int shipsSize5 = 0;
 
-        //Create the gamebord for player1
+        //Create the gameboard for player1
         System.out.println("Player1 please place your ships");
         GameBoard gameBoard1 = new GameBoard (gameBoardLength, water, ship, hit, miss, shipsSize2, shipsSize3, shipsSize4, shipsSize5 );
         gameBoard1.printGameBoard();
         System.out.println();
 
-        //Create the gamebord for player2
+        //Create the gameboard for player2
         System.out.println("Player2 please place your ships");
         GameBoard gameBoard2 = new GameBoard (gameBoardLength, water, ship, hit, miss, shipsSize2, shipsSize3, shipsSize4, shipsSize5 );
         gameBoard2.printGameBoard();
@@ -31,26 +31,26 @@ public class Main {
         );
         //play the game
         for (;;){
-            //make a shut on gamebord2
+            //make a shot on gameboard2
             System.out.println("Player1 please place your shot");
             gameBoard2.printGameBoard();
             gameBoard2.userTarget();
             gameBoard2.printGameBoard();
 
             if (gameBoard2.gameIsOver()) {
-                //print the "won" massage when the game is over
+                //print the "won" message when the game is over
                 System.out.println("Player1 you win the game");
                 break;
             }
 
-            //make a shut on gamebord1
+            //make a shot on gameboard1
             System.out.println("\nPlayer2 please place your shot");
             gameBoard1.printGameBoard();
             gameBoard1.userTarget();
             gameBoard1.printGameBoard();
 
             if (gameBoard1.gameIsOver()) {
-                //print the "won" massage when the game is over
+                //print the "won" message when the game is over
                 System.out.println("\nPlayer2 you win the game");
                 break;
             }
