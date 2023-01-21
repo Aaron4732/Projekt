5e88305291fc81f128db6a1b4ad1fa22d2d871ee
aaron <aaron-dario.fries@stud.fh-campuswien.ac.at>
2023-01-21 11:55:41 +0100

    Fix gradel and create a config file

---
+++ BattelShip/src/main/java/com/example/battelship/Config.java
@@ -0,0 +1,70 @@
+package com.example.battelship;
+
+
+public class Config {
+    static int shipsSize2 = 0;
+    static int shipsSize3 = 1;
+    static int shipsSize4 = 0;
+    static int shipsSize5 = 0;
+
+    static int shipsTotal = shipsSize2 + shipsSize3 + shipsSize4 + shipsSize5;
+
+    static int shipNumber = shipsSize2 *2  + shipsSize3 *3 + shipsSize4 *4 + shipsSize5 *5;
+
+    //Symbols for the gamebord
+    static char water = '-';
+    static char ship = 'S';
+    static char hit = 'X';
+    static char miss = 'O';
+
+    static int gameBoardLength = 4;
+    static int GameBoardHight = 4;
+    public static int getShipsSize2() {
+        return shipsSize2;
+    }
+    public static int getShipsSize3() {
+        return shipsSize3;
+    }
+
+    public static int getShipsSize4() {
+        return shipsSize4;
+    }
+
+    public static int getShipsSize5() {
+        return shipsSize5;
+    }
+
+    public static int getShipsTotal() {
+        return shipsTotal;
+    }
+
+    public static int getShipNumber() {
+        return shipNumber;
+    }
+
+    public static char getWater() {
+        return water;
+    }
+
+    public static char getShip() {
+        return ship;
+    }
+
+    public static char getHit() {
+        return hit;
+    }
+
+    public static char getMiss() {
+        return miss;
+    }
+
+    public static int getGameBoardLength() {
+        return gameBoardLength;
+    }
+
+    public static int getGameBoardHight() {
+        return GameBoardHight;
+    }
+}
+
+
--- BattelShip/src/main/java/com/example/battelship/Main.java
+++ BattelShip/src/main/java/com/example/battelship/Main.java
@@ -2,22 +2,9 @@ package com.example.battelship;
 
 public class Main {
     public static void main(String[] args) {
-        int gameBoardLength = 4;
-
-        //Symbols for the gamebord
-        char water = '-';
-        char ship = 'S';
-        char hit = 'X';
-        char miss = 'O';
-
-        //Set the number of ship availability ships
-        int shipsSize2 = 0;
-        int shipsSize3 = 1;
-        int shipsSize4 = 0;
-        int shipsSize5 = 0;
 
         //Create the gamebord for player1
-        GameBoard gameBoard1 = new GameBoard (gameBoardLength, water, ship, hit, miss, shipsSize2, shipsSize3, shipsSize4, shipsSize5 );
+        GameBoard gameBoard1 = new GameBoard();
         gameBoard1.printGameBoard();
 
         //play the game
