d28819b1445760ab6924b3289523971cb40469d2
aaron <aaron-dario.fries@stud.fh-campuswien.ac.at>
2023-01-21 11:58:03 +0100

    Fix gradel and create a config file

--- BattelShip/src/main/java/com/example/battelship/Main.java
+++ BattelShip/src/main/java/com/example/battelship/Main.java
@@ -3,18 +3,44 @@ package com.example.battelship;
 public class Main {
     public static void main(String[] args) {
 
-        //Create the gamebord for player1
+        //Create the gameboard for player1
+        System.out.println("Player1 please place your ships");
         GameBoard gameBoard1 = new GameBoard();
         gameBoard1.printGameBoard();
+        System.out.println();
 
+        //Create the gameboard for player2
+        System.out.println("Player2 please place your ships");
+        GameBoard gameBoard2 = new GameBoard();
+        gameBoard2.printGameBoard();
+        System.out.println(
+
+        );
         //play the game
-        while (!gameBoard1.gameIsOver())
-        {
-            //make a shut on gamebord1
+        for (; ; ) {
+            //make a shot on gameboard2
+            System.out.println("Player1 please place your shot");
+            gameBoard2.printGameBoard();
+            gameBoard2.userTarget();
+            gameBoard2.printGameBoard();
+
+            if (gameBoard2.gameIsOver()) {
+                //print the "won" message when the game is over
+                System.out.println("Player1 you win the game");
+                break;
+            }
+
+            //make a shot on gameboard1
+            System.out.println("\nPlayer2 please place your shot");
+            gameBoard1.printGameBoard();
             gameBoard1.userTarget();
             gameBoard1.printGameBoard();
+
+            if (gameBoard1.gameIsOver()) {
+                //print the "won" message when the game is over
+                System.out.println("\nPlayer2 you win the game");
+                break;
+            }
         }
-        //print the "won" massage when the game is over
-        System.out.println("You Won!");
     }
 }
